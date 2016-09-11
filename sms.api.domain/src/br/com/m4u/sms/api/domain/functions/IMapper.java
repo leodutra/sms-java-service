14e0a637e4755f26054bfaf310d06f8b9cba44aa
leodutra <leodutra.br@gmail.com>
2016-09-11 17:54:27 -0300

    Broken - Implements better result envelop.

--- sms.api.domain/src/br/com/m4u/sms/api/domain/functions/IMapper.java
+++
@@ -1,6 +0,0 @@
-package br.com.m4u.sms.api.domain.functions;
-
-public interface IMapper<TInput, TOutput> {
-	
-	TOutput map(TInput mappable);
-}
--- sms.api.domain/src/br/com/m4u/sms/api/domain/functions/IOperationValidator.java
+++
@@ -1,7 +0,0 @@
-package br.com.m4u.sms.api.domain.functions;
-
-import br.com.m4u.sms.api.domain.valueobjects.OperationResult;
-
-public interface IOperationValidator<TValidatable> {
-	OperationResult<TValidatable> Validate(TValidatable validationTarget);
-}
